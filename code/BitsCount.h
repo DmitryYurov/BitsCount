60871e693c518a3c7251032d2d54440d3a0c17b7
Dmitry Yurov <dm.yurov@gmail.com>
2023-02-14 12:22:45 +0100

    Add include guards

--- code/BitsCount.h
+++ code/BitsCount.h
@@ -1,6 +1,11 @@
+#ifndef __BITS_COUNT__
+#define __BITS_COUNT__
+
 #include <cstdint>
 
 std::uint8_t bruteForce(std::int64_t num);
 std::uint8_t algoKernighan(std::int64_t num);
 std::uint8_t lookupBytes(std::int64_t num);
 std::uint8_t divideAndConquer(std::int64_t num);
+
+#endif
